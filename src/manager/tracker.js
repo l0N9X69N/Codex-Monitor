d7706ff7fdd345289e8abf8750a8ead36603a1a8
Xuân Long <105354604+l0N9X69N@users.noreply.github.com>
2026-08-26 11:06:21 +0700

    phase8: track lightweight rows incrementally

--- src/manager/tracker.js
+++ src/manager/tracker.js
@@ -8,7 +8,8 @@ export class SessionManagerTracker {
     discoveryIntervalMs = 5000,
     processIntervalMs = 2500,
     knownRefreshIntervalMs = 750,
-    selectedTailIntervalMs = 500
+    selectedTailIntervalMs = 500,
+    summaryBootstrapLimit = 8
   } = {}) {
     if (!core) throw new Error('SessionManagerTracker requires core');
     this.core = core;
@@ -18,12 +19,14 @@ export class SessionManagerTracker {
     this.processIntervalMs = processIntervalMs;
     this.knownRefreshIntervalMs = knownRefreshIntervalMs;
     this.selectedTailIntervalMs = selectedTailIntervalMs;
+    this.summaryBootstrapLimit = Math.max(0, Number(summaryBootstrapLimit) || 0);
     this.lastDiscoveryAtMs = Number.NEGATIVE_INFINITY;
     this.lastProcessAtMs = Number.NEGATIVE_INFINITY;
     this.lastKnownRefreshAtMs = Number.NEGATIVE_INFINITY;
     this.lastSelectedTailAtMs = Number.NEGATIVE_INFINITY;
     this.processEvidence = buildProcessEvidence(null);
     this.lastProcessError = null;
+    this.summariesInitialized = false;
   }
 
   async refreshProcessEvidence(nowMs) {
@@ -52,16 +55,25 @@ export class SessionManagerTracker {
     let discovered = false;
     let knownRefreshed = false;
     let selectedTailed = false;
+    let summariesTailed = false;
 
     if (nowMs - this.lastDiscoveryAtMs >= this.discoveryIntervalMs) {
       this.lastDiscoveryAtMs = nowMs;
       this.core.discover({ processEvidence: this.processEvidence });
       discovered = true;
       this.lastKnownRefreshAtMs = nowMs;
+      if (!this.summariesInitialized) {
+        this.core.bootstrapRecentSummaries(this.summaryBootstrapLimit);
+        this.summariesInitialized = true;
+      } else {
+        for (const item of this.core.index) this.core.summaries.ensure(item, { bootstrap: false });
+      }
     } else if (nowMs - this.lastKnownRefreshAtMs >= this.knownRefreshIntervalMs) {
       this.lastKnownRefreshAtMs = nowMs;
       this.core.refreshKnown({ processEvidence: this.processEvidence });
+      this.core.tailSummaries();
       knownRefreshed = true;
+      summariesTailed = true;
     }
 
     if (this.core.selectedId && nowMs - this.lastSelectedTailAtMs >= this.selectedTailIntervalMs) {
@@ -75,9 +87,12 @@ export class SessionManagerTracker {
       processPolled,
       discovered,
       knownRefreshed,
+      summariesTailed,
       selectedTailed,
       sessions: this.core.index,
+      rows: this.core.rows(),
       selected: this.core.selectedModel(),
+      selectedDetail: this.core.selectedDetail(),
       processError: this.lastProcessError
     };
   }
