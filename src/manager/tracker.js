e25e4d9fdde829e3f694368c627b7232755c9e66
Xuân Long <105354604+l0N9X69N@users.noreply.github.com>
2026-08-26 11:33:08 +0700

    phase8: bootstrap sessions before first process correlation

--- src/manager/tracker.js
+++ src/manager/tracker.js
@@ -49,35 +49,48 @@ export class SessionManagerTracker {
     return true;
   }
 
+  initializeSummaries() {
+    if (!this.summariesInitialized) {
+      this.core.bootstrapRecentSummaries(this.summaryBootstrapLimit);
+      this.summariesInitialized = true;
+    } else {
+      for (const item of this.core.index) this.core.summaries.ensure(item, { bootstrap: false });
+    }
+  }
+
   async tick() {
     const nowMs = this.now();
-    const processPolled = await this.refreshProcessEvidence(nowMs);
+    const discoveryDue = nowMs - this.lastDiscoveryAtMs >= this.discoveryIntervalMs;
+    const firstDiscovery = discoveryDue && this.core.index.length === 0;
     let discovered = false;
     let knownRefreshed = false;
     let selectedTailed = false;
     let summariesTailed = false;
+    let processPolled = false;
 
-    if (nowMs - this.lastDiscoveryAtMs >= this.discoveryIntervalMs) {
+    if (firstDiscovery) {
       this.lastDiscoveryAtMs = nowMs;
-      this.core.discover({ processEvidence: this.processEvidence });
+      this.core.discover();
       discovered = true;
       this.lastKnownRefreshAtMs = nowMs;
-      if (!this.summariesInitialized) {
-        this.core.bootstrapRecentSummaries(this.summaryBootstrapLimit);
-        this.summariesInitialized = true;
-      } else {
-        for (const item of this.core.index) this.core.summaries.ensure(item, { bootstrap: false });
-      }
-      if (processPolled) {
-        await this.refreshProcessEvidence(nowMs + this.processIntervalMs);
-        this.core.refreshKnown({ processEvidence: this.processEvidence });
-      }
-    } else if (nowMs - this.lastKnownRefreshAtMs >= this.knownRefreshIntervalMs) {
-      this.lastKnownRefreshAtMs = nowMs;
+      this.initializeSummaries();
+      processPolled = await this.refreshProcessEvidence(nowMs);
       this.core.refreshKnown({ processEvidence: this.processEvidence });
-      this.core.tailSummaries();
-      knownRefreshed = true;
-      summariesTailed = true;
+    } else {
+      processPolled = await this.refreshProcessEvidence(nowMs);
+      if (discoveryDue) {
+        this.lastDiscoveryAtMs = nowMs;
+        this.core.discover({ processEvidence: this.processEvidence });
+        discovered = true;
+        this.lastKnownRefreshAtMs = nowMs;
+        this.initializeSummaries();
+      } else if (nowMs - this.lastKnownRefreshAtMs >= this.knownRefreshIntervalMs) {
+        this.lastKnownRefreshAtMs = nowMs;
+        this.core.refreshKnown({ processEvidence: this.processEvidence });
+        this.core.tailSummaries();
+        knownRefreshed = true;
+        summariesTailed = true;
+      }
     }
 
     if (this.core.selectedId && nowMs - this.lastSelectedTailAtMs >= this.selectedTailIntervalMs) {
